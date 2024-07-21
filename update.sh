66e2a6d74762dce3e68a5f10c3e236f3a8138a30
billkit <39383468+billkit@users.noreply.github.com>
2024-07-22 01:39:57 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # 获取IP列表文件
-wget -O cn.txt https://ispip.clang.cn/all_cn_cidr.txt
+wget -O all_cn_cidr.txt https://ispip.clang.cn/all_cn_cidr.txt
+wget -O hk.txt https://ispip.clang.cn/hk.txt
+wget -O mo.txt https://ispip.clang.cn/mo.txt
+wget -O tw.txt https://ispip.clang.cn/tw.txt
+
+
 
 # 输入文件路径
-cidr_file="cn.txt"
+cidr_files=("all_cn_cidr.txt" "hk.txt" "mo.txt" "tw.txt")
 
 # 输出RSC文件路径
 rsc_file="CN.rsc"
@@ -15,13 +20,15 @@ address_list="CN"
 # 清空输出文件内容
 > $rsc_file
 
-# 读取TXT文件中的CIDR格式IP地址列表
-while IFS= read -r cidr_address; do
-  # 检查行是否包含 #
-  if ! echo "$cidr_address" | grep -q '#'; then
-    # 将CIDR地址添加到RSC文件中
-    echo "/ip firewall address-list add list=$address_list address=$cidr_address" >> $rsc_file
-  fi
-done < "$cidr_file"
+# 读取所有TXT文件中的CIDR格式IP地址列表
+for cidr_file in "${cidr_files[@]}"; do
+  while IFS= read -r cidr_address; do
+    # 检查行是否包含 #
+    if ! echo "$cidr_address" | grep -q '#'; then
+      # 将CIDR地址添加到RSC文件中
+      echo "/ip firewall address-list add list=$address_list address=$cidr_address" >> $rsc_file
+    fi
+  done < "$cidr_file"
+done
 
 echo "处理完成，CIDR格式的IP地址已转换为RSC格式并写入 $rsc_file"
