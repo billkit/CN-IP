21bd0861bc71ef423c9c494c9f31b6fb2c034347
billkit <39383468+billkit@users.noreply.github.com>
2024-07-22 01:03:26 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,24 +2,25 @@
 
 wget -O chnroutes.txt https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt
 
-# TXT文件的路径
+# 输入文件路径
 cidr_file="chnroutes.txt"
 
-# RSC文件的路径
+# 输出RSC文件路径
 rsc_file="CN.rsc"
 
 # CN地址列表的名称
 address_list="CN"
 
+# 清空输出文件内容
+> $rsc_file
+
 # 读取TXT文件中的CIDR格式IP地址列表
 while IFS= read -r cidr_address; do
-  # 分割CIDR地址为网络和子网掩码
-  network=$(echo $cidr_address | cut -d '/' -f 1)
-  subnet_mask=$(echo $cidr_address | cut -d '/' -f 2)
+  # 检查行是否包含 #
+  if ! echo "$cidr_address" | grep -q '#'; then
+    # 将CIDR地址添加到RSC文件中
+    echo "/ip firewall address-list add list=$address_list address=$cidr_address" >> $rsc_file
+  fi
+done < "$cidr_file"
 
-  # 创建RSC格式的地址
-  rsc_address="${network}/${subnet_mask}"
-
-  # 添加RSC地址到CN地址列表
-  echo "/ip/firewall/address-list add address=${rsc_address} list=CN" >> $rsc_file
-done < $cidr_file
+echo "处理完成，CIDR格式的IP地址已转换为RSC格式并写入 $rsc_file"
