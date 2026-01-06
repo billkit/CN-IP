a4c7061d10da20d796d3acdd4ea2b0903f776eef
billkit <39383468+billkit@users.noreply.github.com>
2026-01-06 23:37:09 +0800

    添加去重功能
    Added a temporary file to store unique CIDR addresses before generating the RSC file.

--- update.sh
+++ update.sh
@@ -6,8 +6,6 @@ wget -O hk.txt https://ispip.clang.cn/hk.txt
 wget -O mo.txt https://ispip.clang.cn/mo.txt
 wget -O tw.txt https://ispip.clang.cn/tw.txt
 
-
-
 # 输入文件路径
 cidr_files=("all_cn_cidr.txt" "hk.txt" "mo.txt" "tw.txt")
 
@@ -20,15 +18,26 @@ address_list="CN"
 # 清空输出文件内容
 > $rsc_file
 
+# 创建一个临时文件用于存储所有IP地址
+temp_file=$(mktemp)
+
 # 读取所有TXT文件中的CIDR格式IP地址列表
 for cidr_file in "${cidr_files[@]}"; do
   while IFS= read -r cidr_address; do
     # 检查行是否包含 #
     if ! echo "$cidr_address" | grep -q '#'; then
-      # 将CIDR地址添加到RSC文件中
-      echo "/ip firewall address-list add list=$address_list address=$cidr_address" >> $rsc_file
+      # 将CIDR地址写入临时文件
+      echo "$cidr_address" >> "$temp_file"
     fi
   done < "$cidr_file"
 done
+
+# 去重并生成RSC文件
+sort -u "$temp_file" | while IFS= read -r unique_cidr_address; do
+  echo "/ip firewall address-list add list=$address_list address=$unique_cidr_address" >> $rsc_file
+done
+
+# 删除临时文件
+rm "$temp_file"
 
 echo "处理完成，CIDR格式的IP地址已转换为RSC格式并写入 $rsc_file"
